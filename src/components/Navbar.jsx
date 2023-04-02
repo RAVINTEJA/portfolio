1a4fd0819495a580180a43e0e4e6313fc21181ef
RAVINTEJA <ravinteja.chilukamari@gmail.com>
2023-04-02 22:05:48 +0530

    Included required Icons in Navbar with linkes to social media

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,15 @@
 import Link from "next/link";
 import Logo from "./Logo.js";
 import { useRouter } from "next/router.js";
+import {
+  GithubIcon,
+  LinkedInIcon,
+  SunIcon,
+  MoonIcon,
+  LinkArrow,
+  DribbbleIcon,
+} from "./Icons.js";
+import { motion } from "framer-motion";
 // React Navigation bar component using Tailwind CSS
 
 const CustomLink = ({ href, title, className = "" }) => {
@@ -9,8 +18,9 @@ const CustomLink = ({ href, title, className = "" }) => {
   return (
     <Link href={href} className={`${className} relative group`}>
       {title}
-      <span className={`h-[1px] inline-block bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300
-       ${router.asPath === href ? 'w-full' : 'w-0'}`}
+      <span
+        className={`h-[1px] inline-block bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300
+       ${router.asPath === href ? "w-full" : "w-0"}`}
       >
         &nbsp;
       </span>
@@ -26,13 +36,34 @@ export default function Navbar() {
         <CustomLink href="/about" title="About" className="mx-4" />
         <CustomLink href="/projects" title="Projects" className="ml-4" />
       </nav>
-      <nav>
-        <Link href="/" target={"_blank"}>
-          Login
-        </Link>
-        <Link href="/" target={"_blank"}>
-          Register
-        </Link>
+      <nav className="flex items-center justify-center flex-wrap">
+        <motion.a
+          href="https://github.com/RAVINTEJA"
+          target={"_blank"}
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+          className="w-6 mx-3"
+        >
+          <GithubIcon />
+        </motion.a>
+        <motion.a
+          href="https://www.linkedin.com/in/ravin-teja-chilukamari-8510bb1b7/"
+          target={"_blank"}
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+          className="w-6 mx-3"
+        >
+          <LinkedInIcon />
+        </motion.a>
+        <motion.a
+          href="/"
+          target={"_blank"}
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+          className="w-6 ml-3"
+        >
+          <DribbbleIcon />
+        </motion.a>
       </nav>
       <div className="absolute left-[50%] top-2 translate-x-[-50%">
         <Logo />
