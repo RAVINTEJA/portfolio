ec83ca582eea34f5db5ef66a5c79008a21609ebc
RAVINTEJA <ravinteja.chilukamari@gmail.com>
2023-04-02 21:20:53 +0530

    Designed the LOGO and more strctured Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,42 @@
 import Link from "next/link";
-import React from "react";
+import Logo from "./Logo.js";
+import { useRouter } from "next/router.js";
 // React Navigation bar component using Tailwind CSS
-const Navbar = () => {
+
+const CustomLink = ({ href, title, className = "" }) => {
+  const router = useRouter();
+
   return (
-    <header className="w-full px-32 py-8  font-medium flex items-center justify-between">
-        <nav>
-            <Link href="/">Home</Link>
-            <Link href="/">About</Link>
-            <Link href="/">Projects</Link>
-        </nav>
-        <h2>Logo</h2>
-        <nav>
-            <Link href="/" target={"_blank"}>Login</Link>
-            <Link href="/" target={"_blank"}>Register</Link>
-        </nav>
-    </header>
+    <Link href={href} className={`${className} relative group`}>
+      {title}
+      <span className={`h-[1px] inline-block bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300
+       ${router.asPath === href ? 'w-full' : 'w-0'}`}
+      >
+        &nbsp;
+      </span>
+    </Link>
   );
 };
 
-// const Navbar = () => {
-//   return (
-//     <div className="w-full ">Navbar</div>
-//   )
-// }
-
-export default Navbar;
+export default function Navbar() {
+  return (
+    <header className="w-full px-32 py-8  font-medium flex items-center justify-between">
+      <nav>
+        <CustomLink href="/" title="Home" className="mr-4" />
+        <CustomLink href="/about" title="About" className="mx-4" />
+        <CustomLink href="/projects" title="Projects" className="ml-4" />
+      </nav>
+      <nav>
+        <Link href="/" target={"_blank"}>
+          Login
+        </Link>
+        <Link href="/" target={"_blank"}>
+          Register
+        </Link>
+      </nav>
+      <div className="absolute left-[50%] top-2 translate-x-[-50%">
+        <Logo />
+      </div>
+    </header>
+  );
+}
