9918fce3af2bf61495912fbbf735eae9ca4b3da1
RAVINTEJA <ravinteja.chilukamari@gmail.com>
2023-04-03 11:19:17 +0530

    Create main part of website and included images and link for my resume using n=button links with option to download

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,7 +30,7 @@ const CustomLink = ({ href, title, className = "" }) => {
 
 export default function Navbar() {
   return (
-    <header className="w-full px-32 py-8  font-medium flex items-center justify-between">
+    <header className="w-full px-32 py-8  font-medium flex items-center bg-light justify-between">
       <nav>
         <CustomLink href="/" title="Home" className="mr-4" />
         <CustomLink href="/about" title="About" className="mx-4" />
