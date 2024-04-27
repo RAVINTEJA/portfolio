02bb1b6620728a00f9ee93d68a3fa9dcce44f5b6
RAVINTEJA <ravinteja.chilukamari@gmail.com>
2024-04-27 10:53:48 +0530

    Refactored CSS classes in HireMe.js and index.js for improved readability and maintainability

--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -5,14 +5,12 @@ import Link from 'next/link'
 const HireMe = () => {
   return (
     <div
-    className='fixed bottom-4 left-4
-    flex items-center justify-center overflow-hidden'
+    className='fixed flex items-center justify-center overflow-hidden bottom-4 left-4'
     >
-        <div className='w-48  h-auto flex items-center justify-center relative'>
+        <div className='relative flex items-center justify-center w-48 h-auto'>
             <CircularText className={"fill-dark dark:fill-light animate-spin-slow"}/>
-            <Link href="mailto:ravinteja.chilukamari@gmail.com"
-            target={"_blank"} className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 flex items-center justify-center bg-dark dark:bg-light text-light dark:text-dark shadow-md border border-solid border-dark dark:border-light
-            w-20 h-20  rounded-full font-semibold hover:bg-light dark:hover:bg-dark hover:text-dark dark:hover:text-light ">
+            <Link href="mailto:ravintejac@gmail.com"
+            target={"_blank"} className="absolute flex items-center justify-center w-20 h-20 font-semibold -translate-x-1/2 -translate-y-1/2 border border-solid rounded-full shadow-md left-1/2 top-1/2 bg-dark dark:bg-light text-light dark:text-dark border-dark dark:border-light hover:bg-light dark:hover:bg-dark hover:text-dark dark:hover:text-light ">
                 Hire Me</Link>
         </div>
     </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -21,10 +21,10 @@ export default function Home() {
         <meta name="description" content="Portfolio Website" />
       </Head>
       <TransitionEffect />
-      <main className="flex items-center pt-0  text-dark 2xl:-my-16 md:my-0 bg-light  dark:bg-dark dark:text-light w-full min-h-screen">
-        <Layout className=" lg:p-8 lg:pt-0 2xl:pb-8 2xl:px-16 p-32 pt-0">
-          <div className="flex w-full items-start justify-between lg:justify-center md:flex-col">
-            <div className=" w-1/2 ex:hidden p-0 md:inline-block md:w-full">
+      <main className="flex items-center w-full min-h-screen pt-0 text-dark 2xl:-my-16 md:my-0 bg-light dark:bg-dark dark:text-light">
+        <Layout className="p-32 pt-0  lg:p-8 lg:pt-0 2xl:pb-8 2xl:px-16">
+          <div className="flex items-start justify-between w-full lg:justify-center md:flex-col">
+            <div className="w-1/2 p-0  ex:hidden md:inline-block md:w-full">
               <Image
                 src="/images/main.png"
                 alt="Ravin Teja"
@@ -33,19 +33,19 @@ export default function Home() {
                 className="w-full h-full"
               />
             </div>
-            <div className="flex w-1/2 gap-4 flex-col items-center self-center lg:w-full lg:text-center">
+            <div className="flex flex-col items-center self-center w-1/2 gap-4 lg:w-full lg:text-center">
               <AnimatedText
                 text="Turning Vision Into Reality With Code And Design."
-                // className="md:text-xl text-6xl md:text-center text-left "
-                className="inline-block text-6xl font-bold w-full capitalize  text-left  lg:text-center ex:text-6xl md:text-5xl sm:text-3xl xs:text-2xl xl:text-5xl 2xl:text-4xl "
+                // className="text-6xl text-left md:text-xl md:text-center "
+                className="inline-block w-full text-6xl font-bold text-left capitalize lg:text-center ex:text-6xl md:text-5xl sm:text-3xl xs:text-2xl xl:text-5xl 2xl:text-4xl "
               ></AnimatedText>
-              <p className=" lg:text-center md:text-base xs:text-sm  text-left font-medium">
+              <p className="font-medium text-left  lg:text-center md:text-base xs:text-sm">
                 As a skilled full-stack developer, I am dedicated to turning
                 ideas into innovative web applications. Explore my latest
                 projects and articles, showcasing my expertise in React.js and
                 web development.
               </p>
-              <div className="flex items-center lg:self-center self-start mt-2">
+              <div className="flex items-center self-start mt-2 lg:self-center">
                 <Link
                   href="https://drive.google.com/file/d/1z9S5X8lq4nFtA4iRjsAhEFtQEipHFd5m/view?usp=sharing"
                   target={"_blank"}
@@ -58,9 +58,9 @@ export default function Home() {
                 >
                   Resume <LinkArrow className={"w-6 ml-1"} />
                 </Link>
-                <Link href="mailto:ravinteja.chilukamari@gmail.com"
+                <Link href="mailto:ravintejac@gmail.com"
                   target={"_blank"}
-                  className="ml-4 text-lg font-medium capitalize text-dark dark:text-light underline"
+                  className="ml-4 text-lg font-medium underline capitalize text-dark dark:text-light"
                 >
                   Contact
                 </Link>
@@ -69,12 +69,12 @@ export default function Home() {
           </div>
         </Layout>
         <HireMe />
-        <div className="absolute md:top-4 md:right-4 bottom-8 right-8 w-24 h-max lg:w-20 exmd:w-12 xs:w-8 inline-block bg-light dark:bg-dark">
+        <div className="absolute inline-block w-24 md:top-4 md:right-4 bottom-8 right-8 h-max lg:w-20 exmd:w-12 xs:w-8 bg-light dark:bg-dark">
           <button
             onClick={() =>
               setTheme(theme === "dark" ? "light" : "dark")
             }
-            className="ml-3 sm:ml-2 flex items-center justify-center rounded-full p-1  bg-light dark:bg-dark"
+            className="flex items-center justify-center p-1 ml-3 rounded-full sm:ml-2 bg-light dark:bg-dark"
           >
             {theme === "dark" ? (
               <Image src={LightBulb} alt="Light Bulb" className="w-full h-auto" onClick={() =>
